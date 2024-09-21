380f0536b4f0f4459214099989d1590516c24783
CVR <chennareddyvenkat06@gmail.com>
2024-09-21 19:42:15 -0400

    fifth commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.awt.desktop.SystemSleepEvent;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!,Hello Venkat");
         System.out.println("Hello India");
         System.out.println("Hello India,USA,Germany");
 
