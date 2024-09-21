d3772901dbbf18e14712964b9ee45dbc21efdda6
CHENNAREDDY06 <147282118+CHENNAREDDY06@users.noreply.github.com>
2024-09-21 19:40:50 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import java.awt.desktop.SystemSleepEvent;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!,Hello Venkat");
-        System.out.println("Hello India");
-        System.out.println("Hello India,USA,Germany");
-
     }
 
 }
