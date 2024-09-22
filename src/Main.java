948ce58badeab30285ec95887a1565fa43a0d6f0
CVR <chennareddyvenkat06@gmail.com>
2024-09-21 22:18:52 -0400

    eight commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello India!");
 
-        System.out.println("Hello INDIA...!");
+        System.out.println("Hello USA!");
 
 
     }
