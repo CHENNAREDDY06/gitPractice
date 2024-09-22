b7f5dfb35a76fd477d8150d98f820e4506c83b46
CHENNAREDDY06 <147282118+CHENNAREDDY06@users.noreply.github.com>
2024-09-21 22:58:34 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.awt.desktop.SystemSleepEvent;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I am testing merging !");
-        System.out.println("these are changes from feature!");
+        System.out.println("ABC");
+       
 
 
 
