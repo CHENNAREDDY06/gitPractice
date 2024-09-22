e445c64dc58dd6e0f8ac9d18b518711bef021782
CHENNAREDDY06 <147282118+CHENNAREDDY06@users.noreply.github.com>
2024-09-22 08:13:57 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("STUVW");
         System.out.println("");
         System.out.println("xyz");
-        System.out.println("XYZ");
+        System.out.println("123");
 
 
     }
