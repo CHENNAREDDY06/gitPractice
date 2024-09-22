74d48d74cddce61402fe9d5dbb08430608582740
CHENNAREDDY06 <147282118+CHENNAREDDY06@users.noreply.github.com>
2024-09-21 23:26:31 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         System.out.println("JKLM");
         System.out.println("OPQR");
-        System.out.println("STUV");
+        System.out.println("STUVW");
 
     }
 
